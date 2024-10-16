e3cc59acd6d57763ffc1851c003af54717eb497a
Ibrahim Aziz <47850595+mbahnapi@users.noreply.github.com>
2024-10-16 11:05:24 +0700

    Update nokia-isam.inc.php
    update port counting

--- includes/polling/ports/os/nokia-isam.inc.php
+++ includes/polling/ports/os/nokia-isam.inc.php
@@ -26,5 +26,31 @@ $isam_port_stats = snmpwalk_cache_oid($device, 'asamIfExtCustomerId', [], 'ITF-M
 
 foreach ($isam_port_stats as $index => $value) {
     $port_stats[$index]['ifAlias'] = $isam_port_stats[$index]['asamIfExtCustomerId'];
+	
+foreach (array ( 'ifHCInOctets','ifHCInUcastPkts','ifHCInMulticastPkts','ifHCInBroadcastPkts','ifHCOutOctets','ifHCOutUcastPkts','ifHCOutMulticastPkts','ifHCOutBroadcastPkts') as $oid_HEX_Object) {	
+		$port_stats[$index][$oid_HEX_Object] = hexdecs(preg_replace("/[^0-9A-Fa-f]/", '', $port_stats[$index][$oid_HEX_Object]));
+	}
+
 }
 unset($isam_ports_stats);
+
+// -- SOURCE https://www.php.net/manual/en/function.hexdec.php
+function hexdecs($hex)
+{
+    // ignore non hex characters
+    $hex = preg_replace('/[^0-9A-Fa-f]/', '', $hex);
+   
+    // converted decimal value:
+    $dec = hexdec($hex);
+   
+    // maximum decimal value based on length of hex + 1:
+    //   number of bits in hex number is 8 bits for each 2 hex -> max = 2^n
+    //   use 'pow(2,n)' since '1 << n' is only for integers and therefore limited to integer size.
+    $max = pow(2, 4 * (strlen($hex) + (strlen($hex) % 2)));
+   
+    // complement = maximum - converted hex:
+    $_dec = $max - $dec;
+   
+    // if dec value is larger than its complement we have a negative value (first bit is set)
+    return $dec > $_dec ? -$_dec : $dec;
+}
